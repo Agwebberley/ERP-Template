31ec16503ba93f12b459e5265e291ad88c50dbb4
Agwebberley <agwebberley@pm.me>
2024-06-19 12:05:11 -0700

    serialize functions

--- ERP-Template/orders/serializers/order_serializer.py
+++ ERP-Template/orders/serializers/order_serializer.py
@@ -3,17 +3,13 @@ from rest_framework import serializers
 from orders.models import order
 
 
+from orders.serializers.order_item_serializer import order_itemSerializer
 from orders.models import order_item
-
-
-class order_itemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = order_item
-        fields = '__all__'
-
 
 class orderSerializer(serializers.ModelSerializer):
     order_item = order_itemSerializer(many=True)
+    total = serializers.DecimalField(source="get_total_price", max_digits=10, decimal_places=2, read_only=True)
+
 
     class Meta:
         model = order
@@ -30,7 +26,6 @@ class orderSerializer(serializers.ModelSerializer):
         order_items_data = validated_data.pop('order_item')
         instance.customer_id = validated_data.get('customer_id', instance.customer_id)
         instance.order_date = validated_data.get('order_date', instance.order_date)
-        instance.total_amount = validated_data.get('total_amount', instance.total_amount)
         instance.status = validated_data.get('status', instance.status)
         instance.save()
 
@@ -41,9 +36,9 @@ class orderSerializer(serializers.ModelSerializer):
                 order_item = order_item.objects.get(id=order_item_id, order=instance)
                 order_item.part_id = item_data.get('part_id', order_item.part_id)
                 order_item.quantity = item_data.get('quantity', order_item.quantity)
-                order_item.unit_price = item_data.get('unit_price', order_item.unit_price)
                 order_item.save()
             else:
                 order_item.objects.create(order_id=instance, **item_data)
         
         return instance
+
