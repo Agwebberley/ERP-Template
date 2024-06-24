ace0c881819de5cf411c74e4200395e734d79df9
Agwebberley <agwebberley@pm.me>
2024-06-24 16:49:17 -0700

    graphene optimizer

--- ERP-Template/core/schema.py
+++ ERP-Template/core/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType
+from graphene_django_optimizer import resolver_hints
 from core.models import Order, Order_item, Customer, Part
 
 
@@ -28,18 +29,23 @@ class PartType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_orders = graphene.List(OrderType)
+    all_order_items = graphene.List(Order_itemType)
+    all_customers = graphene.List(CustomerType)
+    all_parts = graphene.List(PartType)
+
+    @resolver_hints(model_field='orders')
     def resolve_all_orders(self, info, **kwargs):
         return Order.objects.all()
 
-    all_order_items = graphene.List(Order_itemType)
+    @resolver_hints(model_field='order_items')
     def resolve_all_order_items(self, info, **kwargs):
         return Order_item.objects.all()
 
-    all_customers = graphene.List(CustomerType)
+    @resolver_hints(model_field='customers')
     def resolve_all_customers(self, info, **kwargs):
         return Customer.objects.all()
 
-    all_parts = graphene.List(PartType)
+    @resolver_hints(model_field='parts')
     def resolve_all_parts(self, info, **kwargs):
         return Part.objects.all()
 
