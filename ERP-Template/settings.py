cd7880bb678c11633bb69498211eeadf71829064
Agwebberley <agwebberley@pm.me>
2024-07-04 11:38:19 -0700

    Shift Dir

--- ERP-Template/settings.py
+++ ERP-Template/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*x94-xr4db_81kog*syb5o!tlgo)6)r@ib(&$0amfca=tr-=#q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'django-env.eba-ebpb4enp.us-west-2.elasticbeanstalk.com']
 
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
