ba451848e41a8f0affce2f2daaea010b6e109312
Agwebberley <agwebberley@pm.me>
2024-07-11 22:16:46 -0700

    set -e?

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
-python3 manage.py makemigrations --noinput customers orders inventory finance core
+set -e
+python3 manage.py makemigrations customers orders inventory finance core
 python3 manage.py migrate
 python3 manage.py update_config
 python3 manage.py fake_data
